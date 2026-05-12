09ac0e74c9abef31438fc77a6744a28f77e27e18
Margaret Samuel <clementpmargreth@gmail.com>
2026-05-12 12:13:14 +0100

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,3 +12,30 @@ Route::view('dashboard', 'dashboard')
     ->name('dashboard');
 
 require __DIR__.'/settings.php';
+
+
+Route::get('run-seeder', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+    return 'Seeder executed successfully!';
+});
+
+Route::get('migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migration executed successfully!';
+});
+
+Route::get('route-cache', function () {
+    Artisan::call('route:cache');
+    return 'Route cache cleared successfully!';
+});
+
+Route::get('config-clear', function () {
+    Artisan::call('config:clear');
+    return 'Config cache cleared successfully!';
+});
+
+Route::get('config-cache', function () {
+    Artisan::call('config:cache');
+    return 'Config cached successfully!';
+});
+
